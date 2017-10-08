0cbd2beaf3a8934c03b70e4310c7d53024dbdeab
Brice Burgess <nesta@iceburg.net>
2017-10-08 17:01:09 -0400

    init: configure gpg-agent as ssh-agent

--- init.d/00-gnupg.sh
+++ init.d/00-gnupg.sh
@@ -10,6 +10,8 @@ init/00-gnupg(){
   p/log "import $MASTER_KEY_ID 'laptop' subkeys"
   gnupg/import "$BOOTY_TMPDIR/master.subkeys"
 
+  gnupg/gpg-agent
+
   p/log "import $MASTER_KEY_ID secret key"
   prompt/confirm "continue importing MASTER KEY? usually no." || return 0
   gnupg/import "$BOOTY_TMPDIR/master.key"
@@ -35,4 +37,34 @@ gnupg/import(){
   done
 
   cat ${files[@]} | "$BOOTY_ROOT/bin/bcrypt" > "$file" &&  gpg --import "$file"
+}
+
+
+gnupg/gpg-agent(){
+  p/log "configure gpg-agent as ssh-agent"
+  file/interpolate '^enable-ssh-support.*$' \
+                   'enable-ssh-support' "$HOME/.gnupg/gpg-agent.conf"
+  file/interpolate '^default-cache-ttl.*$' \
+                   'default-cache-ttl 10800' "$HOME/.gnupg/gpg-agent.conf"
+
+  file/interpolate '^SSH_AGENT_PID.*$' \
+                   'SSH_AGENT_PID	DEFAULT=' "$HOME/.pam_environment"
+  file/interpolate '^default-cache-ttl.*$' \
+                   'SSH_AUTH_SOCK	DEFAULT="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh' "$HOME/.pam_environment"
+
+  file/interpolate '^export GPG_TTY=.*$' \
+                   'export GPG_TTY=$(tty)' "$HOME/.bashrc"
+  file/interpolate '^gpg-connect-agent updatestartuptty.*$' \
+                   'gpg-connect-agent updatestartuptty /bye >/dev/null' "$HOME/.bashrc"
+
+  p/log "add authentication key to gpg-agent"
+  AUTHKEY_KEYGRIP="${AUTHKEY_KEYGRIP:-$(gpg --with-keygrip -k $MASTER_KEY_ID | grep -A1 -B0 '\[A\]' | tail -n1 |  awk '{print $NF}')}"
+  file/interpolate "^$AUTHKEY_KEYGRIP.*$" "$AUTHKEY_KEYGRIP" "$HOME/.gnupg/sshcontrol"
+
+  p/log "reload gpg-agent"
+  gpg-connect-agent reloadagent /bye
+
+  p/log "export ~/.ssh/gpg-agent-keys.pub (for use as authorized keys)"
+  [ -d "$HOME/.ssh" ] || { mkdir "$HOME/.ssh"; chmod 0700 "$HOME/.ssh" ;}
+  ssh-add -L > "$HOME/.ssh/gpg-agent-keys.pub"
 }
