0bb0e212af03827f9a233866d77eef6c7fc404b2
Brice Burgess <nesta@iceburg.net>
2017-11-05 11:43:45 -0500

    xorg: base install + track rc via dotfiles

---
+++ init.d/30-xorg.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env shell-helpers
+
+xorg(){
+  xorg/$BOOTY_DISTRO
+
+  file/interpolate '^for f in ~/\.xinit\.d/.*$' \
+    "for f in ~/.xinit.d/*; do source \"\$f\"; done" "$HOME/.xinitrc"
+}
+
+xorg/archlinux(){
+  pkgs=(
+    acpi
+    chromium
+    xmonad
+    xmonad-contrib
+    xorg-apps
+    xorg-server
+    xorg-xinit
+    mesa
+    mesa-demos
+  )
+
+  case "$BOOTY_ENV" in
+    hartford)
+      pkgs+=( xf86-video-amdgpu mesa-vdpau ) ;;
+    toshiba)
+      pkgs+=( xf86-video-intel ) ;;
+    *)
+      p/warn "unsupported environment: $BOOTY_ENV" ;;
+  esac
+
+  sudo pacman --noconfirm -S ${pkgs[@]}
+}
