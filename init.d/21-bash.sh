fa25fc60a699afaddd9ffd3c84a47465fc5849a0
Brice Burgess <nesta@iceburg.net>
2017-11-05 12:34:27 -0500

    bash: completions and fuzzy finders

--- init.d/21-bash.sh
+++ init.d/21-bash.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env shell-helpers
 
 bash(){
+  bash/$BOOTY_DISTRO
+
   file/interpolate '^for f in ~/\.bash\.d/.*$' \
     "for f in ~/.bash.d/*; do source \"\$f\"; done" "$HOME/.bashrc"
+}
+
+bash/archlinux(){
+  pkgs=(
+    bash-completion
+    fzf
+    the_silver_searcher
+  )
+  sudo pacman --noconfirm -S ${pkgs[@]}
 }
