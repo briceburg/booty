f48c4a10b8843fd7dea21f93853351220f13dfbb
Brice Burgess <nesta@iceburg.net>
2026-07-03 11:11:47 -0600

    Enable PipeWire user services in Arch bootstrap

--- bootstrap/archlinux/20-configure.sh
+++ bootstrap/archlinux/20-configure.sh
@@ -5,14 +5,33 @@ if ((${#BOOTSTRAP_AUR[@]})); then
   as_user "$BOOTSTRAP_USER" "$aur_install" "${BOOTSTRAP_AUR[@]}"
 fi
 
+user_systemctl() {
+  local user_uid
+
+  user_uid="$(id -u "$BOOTSTRAP_USER")"
+  loginctl enable-linger "$BOOTSTRAP_USER"
+  systemctl start "user@$user_uid.service"
+  as_user "$BOOTSTRAP_USER" env \
+    "XDG_RUNTIME_DIR=/run/user/$user_uid" \
+    systemctl --user "$@"
+}
+
 [ ! -e /run/systemd/resolve/stub-resolv.conf ] || ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 ln -sf "/usr/share/zoneinfo/$BOOTSTRAP_TIMEZONE" /etc/localtime
 ln -sf /usr/bin/vim /usr/bin/vi
 
 if ! systemctl list-units >/dev/null 2>&1; then
   log "skipping service enablement: systemd is not running"
-elif ((${#BOOTSTRAP_SERVICES[@]})); then
-  systemctl enable --now "${BOOTSTRAP_SERVICES[@]}"
+else
+  if ((${#BOOTSTRAP_SERVICES[@]})); then
+    log "enabling system services: ${BOOTSTRAP_SERVICES[*]}"
+    systemctl enable --now "${BOOTSTRAP_SERVICES[@]}"
+  fi
+
+  if ((${#BOOTSTRAP_USER_SERVICES[@]})); then
+    log "enabling user services for $BOOTSTRAP_USER: ${BOOTSTRAP_USER_SERVICES[*]}"
+    user_systemctl enable --now "${BOOTSTRAP_USER_SERVICES[@]}"
+  fi
 fi
 
 if has steam "${BOOTSTRAP_FEATURES[@]}"; then
